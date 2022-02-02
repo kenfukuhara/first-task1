af6e545229fba7b93bbfdcdbf1ab0a209c86059b
福原健 <fukuharaken@fukuharakennoMacBook-Air.local>
2022-02-02 12:30:21 +0900

    ２回目のコミットです

---
+++ js/dom-script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function({
+  const getNode = this.document.getElementByld("get-node");
+  console.log(getNode);
+  getNode.append("追加しました")
+  getNode.innerHTML = `<h1>javaScriptの勉強</h1>`
+  getNode.remove();
+
+
+}))
---
+++ js/task-javascript.js
@@ -0,0 +1,2 @@
+const getNode = document.getElementById("techgeek-header");
+console.log("テックギーク")
