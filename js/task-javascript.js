ef1f0e5d25fc0c388d5a46aaeccc807b0521c7a8
福原健 <fukuharaken@fukuharakennoMacBook-Air.local>
2022-02-04 12:48:33 +0900

    できました

--- js/task-javascript.js
+++ js/task-javascript.js
@@ -1,10 +1,12 @@
 
-window.addEventListener(“click”, function() {
-  const button = document.getElementById("click-change");
-  console.log(button);
-
-  button.click = "テックギーク";
-  
-})
 
 
+window.addEventListener("click",function(){
+  const btn = document.getElementById("click-change");
+  btn.innerHTML = `<h1>JavaScriptの勉強</h1>`;
+});
+
+window.addEventListener("click",function(){
+  const header = document.getElementById("techgeek-header")
+  header.append("クリックされました");
+});
